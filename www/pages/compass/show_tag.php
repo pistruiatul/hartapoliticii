835135a6536c51741c99a7859e5b21171b5d4fcd
Octavian Costache <octavian.costache@gmail.com>
2012-02-11 12:46:57 -0500

    Changes to the tags pages to look better

--- www/pages/compass/show_tag.php
+++ www/pages/compass/show_tag.php
@@ -3,15 +3,18 @@ include_once('hp-includes/person_class.php');
 include_once('hp-includes/people_util.php');
 
 include_once('mods/functions_common.php');
-
+include_once('pages/functions_common.php');
 
 // Just get me a list with the people that are in cdep/2008
 function getPeopleList($room, $year) {
   $s = mysql_query("
-    SELECT people.id, display_name
+    SELECT people.id, display_name, p.name as party_name
     FROM people
     LEFT JOIN people_history AS h ON h.idperson = people.id
-    WHERE h.what = '{$room}/{$year}'
+    LEFT JOIN people_facts AS f ON f.idperson = people.id
+    LEFT JOIN parties AS p ON f.value = p.id
+    WHERE h.what = '{$room}/{$year}' AND
+          f.attribute = 'party'
   ");
 
   $list = array();
@@ -44,6 +47,7 @@ $year = '2008';
 
 $title = 'Tag "' . getTagNameForId($tagid) . '"';
 $nowarning = true;
+
 include('header.php');
 
 if ($csum == md5($room . '_' . $year . '_votes_details' . $uid . $tagid .
@@ -56,9 +60,14 @@ if ($csum == md5($room . '_' . $year . '_votes_details' . $uid . $tagid .
   $t->assign('votes', $votes);
 
   $people = getPeopleList($room, $year);
+
+  $non_zero_people = array();
+  $zero_people = array();
+
   for ($i = 0; $i < sizeof($people); $i++) {
     $c = getBeliefContext($room, $year, $uid, $people[$i]['id'], $tagid,
                           $possible);
+
     $people[$i]['w1'] = $c['w1'];
     $people[$i]['w2'] = $c['w2'];
     $people[$i]['w3'] = $c['w3'];
@@ -70,13 +79,24 @@ if ($csum == md5($room . '_' . $year . '_votes_details' . $uid . $tagid .
 
     // Since I'm here, fix the name. A little hacky.
     $people[$i]['link'] = "?cid=9&id=" . $people[$i]['id'];
+    $people[$i]['tiny_photo'] = getTinyImgUrl($people[$i]['id']);
+
+    if ($c['c2'] + $c['c4']) {
+      array_push($non_zero_people, $people[$i]);
+    } else {
+      array_push($zero_people, $people[$i]);
+    }
   }
 
-  usort($people, "beliefCmp");
-  $t->assign('people', $people);
+  usort($non_zero_people, "beliefCmp");
+  $t->assign('people', $non_zero_people);
+
+  $t->assign('absentees', $zero_people);
 
   $t->assign('user_login', getUserLogin($uid));
   $t->display('compass_show_tag.tpl');
+} else {
+  echo "Wrong checksum";
 }
 
 ?>
