799fdfe71a0a4907c1594e29a2146d2f2e2e1ee0
Octavian Costache <octavian.costache@gmail.com>
2012-02-22 21:14:28 -0500

    Adding a sample db_user file, moving a script out of secret into tools

---
+++ tools/push_to_web.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+set -x
+
+read -s -p "Enter Password: " PASS
+
+# This script is meant to be run from the root of the repository and it pushes
+# the changes from the local repository into production.
+cd ./www
+
+lftp -u hartapoliticii,$PASS www.hartapoliticii.ro <<EOF
+mirror -v -R --ignore-time \
+    -X '*.htaccess' \
+    -X '*.htpasswd' \
+    -X 'a/*' \
+    -X 'templates_c/*' \
+    -X 'images/people/*' \
+    -X 'images/people_tiny/*' \
+    -X '.git/*' \
+    -X '*.DS_Store' \
+    ./ /www/www
+quit 0
+EOF
