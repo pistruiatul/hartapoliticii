c8b15f9db5fe52fc94466868a6d3220e5d80e78a
Octavian Costache <octavian.costache@gmail.com>
2012-04-23 12:03:00 -0400

    Cleaning up the links page

--- www/pages/misc/utile.php
+++ www/pages/misc/utile.php
@@ -11,15 +11,32 @@ include('header.php');
 ?>
 
 <div class="plaintext">
-<strong>Unde vă mai puteți informa</strong>
 <blockquote>
+<strong>Site-uri relevante în 2012</strong>
   <ul>
+    <li>
+      Cine Ce-a Promis - un site care monitorizează promisiunile candidaților
+        <ul>
+          <li>
+            <a href="http://www.cineceapromis.ro/">
+              www.cineceapromis.ro</a>
+          </li>
+        </ul>
+      <br>
+    </li>
     <li>Site-uri cu informații generale despre alegeri și toți candidații:
-      <ul><li><a href="http://www.alegeri-2008.ro">www.alegeri-2008.ro</a></li>
-      <li><a href="http://www.thinkopolis.eu">www.thinkopolis.eu</a></li>
-      <li><a href="http://www.alegeri.tv">www.alegeri.tv</a></li>
-      </ul><br></li>
+      <ul>
+        <li>
+          <a href="http://www.alegeri.tv">www.alegeri.tv</a>
+        </li>
+      </ul><br>
+    </li>
+  </ul>
 
+
+<strong>Site-uri relevante în 2008</strong>
+
+  <ul>
     <li>România Curată - pentru sustinerea caruia colaboreaza 6 ONG-uri si o organizatie sindicala implicate in lupta anticoruptie... <a href="http://www.romaniacurata.ro/despre-noi.php">more here</a>
       <ul><li><a href="http://www.romaniacurata.ro/alegeri-parlamentare-2008.html">Liste negre pe județe</a></li>
         <li><a href="http://www.romaniacurata.ro/despre-noi.php">Cine sunt și ce vor</a></li></ul><br></li>
@@ -28,10 +45,17 @@ include('header.php');
       <ul><li>Raportul pentru zona de vest: <a href="http://tvr.ro/files/41971.pdf">tvr.ro/files/41971.pdf</a></li>
           <li>Raportul pentru zona de sud: <a href="http://tvr.ro/files/41972.pdf">tvr.ro/files/41972.pdf</a></li>
           <li><a href="http://www.qvorum.ro/Default.aspx">Cine sunt Qvorum și ce vor</a></li></ul><br></li>
-      <li>Lista candidaților pătați din Cațavencu, organizați pe județe: <a href="http://www.catavencu.ro/lista.html">http://www.catavencu.ro/lista.html</a><br><br></li>
-      <li>Institutul de Politici Publice: <a href="http://www.ipp.ro">www.ipp.ro</a><br><br></li>
+  </ul>
 
-      <li>De ce să votezi sau nu: <a href="http://www.votezi.ro">www.votezi.ro</a><br><br></li>
+<strong>Site-uri general utile</strong>
+  <ul>
+    <li>Institutul de Politici Publice:
+      <a href="http://www.ipp.ro">www.ipp.ro</a><br><br>
+    </li>
+    <li>Aleșii Votează:
+      <a href="http://www.alesiivoteaza.ro">www.alesiivoteaza.ro</a><br><br>
+    </li>
   </ul>
 </blockquote>
+
 </div>
