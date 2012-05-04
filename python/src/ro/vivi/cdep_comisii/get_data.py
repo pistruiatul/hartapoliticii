15bbcc781920523b62cb1246db8427d8643c3402
Cornel Ghiban <cghiban@gmail.com>
2012-05-04 00:21:52 -0400

    - added code to browse the meetings of the commities TODO: download the documents of the meetings

--- python/src/ro/vivi/cdep_comisii/get_data.py
+++ python/src/ro/vivi/cdep_comisii/get_data.py
@@ -48,6 +48,48 @@ def get_comisii(tmp_dir):
 
   return Set(reg_comisii.findall(comisii_page))
 
+def get_meetings(com_id, year, tmp_dir):
+  link = 'http://www.cdep.ro/co/sedinte.lista?tip=%s&an=%s' % (com_id, year) # 2011, 2010, 2009
+  print " + %s" % link
+  sedinte_page = get_page(link, tmp_dir)
+  #reg_date = re.compile('<span class="title_gradient_1">(.*?)</span>')
+  #ro_dates = reg_date.findall(html)
+
+  meetings = []
+  soup = BeautifulSoup(sedinte_page)
+  div=soup.find('div', attrs={'id':'pageContent'})
+  ps = div.findNextSiblings('p')
+  # check if the <p> containing the table exists
+  if len(ps) == 0:
+    return meetings
+
+  t=ps[0].find('table')
+  tr = t.find('tr')
+  reg_fix_date1 = re.compile("^\d+\.")
+  while tr is not None and hasattr(tr, 'name') and getattr(tr, 'name') == 'tr':
+    date = tr.text
+    date = reg_fix_date1.sub('', date)
+    docs = []
+
+    # get the next row (which contains the attached docs)
+    tr = tr.nextSibling.nextSibling
+    tt = tr.find('table')
+    for dtr in tt.findAll('tr'):
+      cells = dtr.findAll('td')
+      name = cells[0].text
+      link = cells[1].find('a')['href']
+
+      docs.append({'name': name, 'link': link})
+
+    #print "\t %s -> %d doc(s)" % (date, len(docs))
+    meetings.append((date, docs))
+    
+    # next row that has the date/time of the meeting
+    tr = tr.nextSibling.nextSibling
+
+  return meetings
+
+
 # get former members
 #   tb - table
 def extract_former_members(tb):
@@ -136,6 +178,34 @@ def get_comisie_details(id, tmp_dir):
   
   return members
 
+def get_bin_file(link, tmp_dir):
+  """ Fetches the binary file (pdf) at the provided link.  
+  """
+
+  _, file_ext = os.path.splitext(link)
+  if ext == '': ext = '.dat'
+
+  # First, see if this is already cached.
+  fname = tmp_dir + '/bin_cache/%s%s' % hashlib.md5(link).hexdigest(), ext
+  if os.path.exists(fname):
+    return get_file_data(fname)
+
+  success = False
+  while not success:
+    try:
+      f = urllib2.urlopen(link, None, 20)
+      data = f.read()
+      f.close()
+
+      # Save into a cached file.
+      cache_file = open(fname, 'w')
+      cache_file.write(data)
+      cache_file.close()
+
+      return data
+    except URLError:
+      print "Timed out, retrying ", link
+      success = False
 
 def main():
   """ Main function. """
@@ -149,13 +219,18 @@ def main():
 
   print "tmp_dir: %s" % TMP_DIR
 
-  comisii = get_comisii(TMP_DIR)
-  print "Found %d comisii\n" % len(comisii)
-  if len(comisii) > 0:
-    for comisie in comisii:
-      #if comisie[0] != '1': continue
-      members = get_comisie_details(comisie[0], TMP_DIR)
-      print "\t%d membri" % len(members)
+  if True:
+    comisii = get_comisii(TMP_DIR)
+    print "Found %d comisii\n" % len(comisii)
+    if len(comisii) > 0:
+      for comisie in comisii:
+        com_id = comisie[0]
+        #if com_id != '2': continue
+        members = get_comisie_details(com_id, TMP_DIR)
+        print "\t%d membri" % len(members)
+        sedinte = get_meetings(com_id, 2011, TMP_DIR)
+        print "\t %d ședințe" % len(sedinte)
+
 
 main()
 
