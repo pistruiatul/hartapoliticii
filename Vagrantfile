35e1e0cf9c8ebdcdc0f81562da135bcd77c3bb5f
Alex Morega <alex@grep.ro>
2012-07-01 18:11:29 +0300

    wip

--- Vagrantfile
+++ Vagrantfile
@@ -6,7 +6,15 @@
 #    $ gem install vagrant
 #    $ vagrant box add lucid32 http://files.vagrantup.com/lucid32.box
 #    $ vagrant up
+
 # Then open http://192.168.33.17/ in a browser, you should have a working site.
+
+# For more information about vagrant, see http://vagrantup.com/
+
+# configure your secret/db_user.php (user: root, password: root)
+
+
+# TODO mount the repository in /hartapoliticii
 
 
 Vagrant::Config.run do |config|
--- vagrant-provision.sh
+++ vagrant-provision.sh
@@ -2,17 +2,36 @@
 
 
 apt-get update
+apt-get install -y vim
 apt-get install -y apache2-mpm-prefork libapache2-mod-php5 php5-mysql
-apt-get install -y mysql-client #mysql-server
+
+
+### apache ###
 
 read -r -d '' MY_APACHE_CFG <<'EOF'
 <VirtualHost *:80>
     DocumentRoot /vagrant/www
 </VirtualHost>
 EOF
-
 echo "$MY_APACHE_CFG" > /etc/apache2/sites-available/hartapoliticii
 
 a2dissite 000-default
 a2ensite hartapoliticii
 /etc/init.d/apache2 reload
+
+
+### database ###
+
+read -r -d '' MYSQL_PRESEED <<'EOF'
+mysql-server-5.0 mysql-server/root_password_again  string root
+mysql-server-5.0 mysql-server/root_password        string root
+EOF
+echo "$MYSQL_PRESEED" > /var/local/preseeding-mysql-server.seed
+
+debconf-set-selections /var/local/preseeding-mysql-server.seed
+apt-get install -y mysql-client mysql-server
+
+# TODO only run once:
+#mysqladmin -u root -proot password ""
+mysql -u root -e "create database hartapoliticii"
+mysql -u root hartapoliticii < /vagrant/db/hartapoliticii_mock_data.sql
