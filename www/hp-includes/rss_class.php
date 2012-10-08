0a93be2e7b3c5dcb116011c3cc54e28f0dbfe638
Andrei Cristian Petcu <andreicristianpetcu@gmail.com>
2012-10-09 01:17:04 +0300

    small RSS changes

--- www/hp-includes/rss_class.php
+++ www/hp-includes/rss_class.php
@@ -1,19 +1,43 @@
 <?php
 
 /**
- * @fileoverview A class identifying a party. This class will contain utilities
- * for getting various information about a party from the database.
+ * @fileoverview A class used for generating RSS feeds. This class will contain utilities
+ * for generating RSS feeds in a generic way.
  */
 class Rss {
 
-  /** The short name of this party. */
+  /** The rss title. */
   public $title;
 
-  /** The long name of this party. */
+  /** The rss link. */
   public $link;
 
-  /** The id of this party in the database. */
+  /** The rss description. */
   public $description;
+  
+  public $rssItems = array();
+  
+  public function __construct($title,$linkPath,$description){
+    $this->title=$title;
+    $this->link=$this->getSiteUrl().$linkPath;
+    $this->description=$description;
+  }
+  
+  public function addRssItem(){
+  	
+  }
+  
+  /**
+   * Returns the site url.
+   * @return {string}
+   */
+  public function getSiteUrl() {
+    return 'http://'.$_SERVER['HTTP_HOST']."/";
+  }
+  
+  public function printRssSmarty(){
+  	$smarty = new Smarty();
+  }
 
 }
 ?>
--- www/pages/rss/rss_person_declarations.php
+++ www/pages/rss/rss_person_declarations.php
@@ -1,9 +1,9 @@
 <?php
 
+include_once('hp-includes/rss_class.php');
 include_once('hp-includes/person_class.php');
 
 $id = $_GET['id'];
-$site_url = 'http://'.$_SERVER['HTTP_HOST']."/";
 
 // Given the ID of a person, go through it's history and load all
 // the mods (modules) in the respective directories.
@@ -26,26 +26,38 @@ function constructUrl($baseUrl, $params, $newParams=array()) {
 }
 
 if (sizeof($declarations) > 0) {
+	
+  
+  $rssTitle = "Declaratii ".$person->displayName;
+  $rssLinkPath = "?name={$person->getNameForUrl()}&exp=person_declarations";
+  $rssDescription = "Fluxul declarațiilor lui ".$person->displayName;
+  $rss = new Rss($rssTitle,$rssLinkPath,$rssDescription);
+  
   $t = new Smarty();
   
-  $rss['title'] = "Declaratii ".$person->displayName;
-  $rss['link'] = $site_url."?name={$person->getNameForUrl()}&exp=person_declarations";
-  $rss['description'] = "Fluxul declarațiilor lui ".$person->displayName;
+  $rss2['title'] = "Declaratii ".$person->displayName;
+  $rss2['link'] = $site_url."?name={$person->getNameForUrl()}&exp=person_declarations";
+  $rss2['description'] = "Fluxul declarațiilor lui ".$person->displayName;
 
   foreach ($declarations as $declaration) {
+  		
   	$date = gmdate(DATE_RSS, strtotime($declaration['time'].time()));
-    $rss_item['title'] = "Declaratie ".$person->displayName." la data de ".$date;
-    $rss_item['description'] = substr($declaration['declaration'], 0, 250)."...";
-	$rss_item['link'] = constructUrl($site_url, array(), array(
+    $rss2_item['title'] = "Declaratie ".$person->displayName." la data de ".$date;
+    $rss2_item['description'] = substr($declaration['declaration'], 0, 250)."...";
+	$rss2_item['link'] = constructUrl($site_url, array(), array(
 	    'name' => $site_url.$person->getNameForUrl(),
     	'exp' => 'person_declarations',
     	'decl_id' => $declaration['id']
   	));
-    $rss_item['pubDate'] = $date;
-    $rss_items[] = $rss_item;
+    $rss2_item['pubDate'] = $date;
+    $rss2_items[] = $rss2_item;
+	
+	
+	$rss->addRssItem()
+	
   }
-  $t->assign('rss', $rss);
-  $t->assign('rss_items', $rss_items);
+  $t->assign('rss', $rss2);
+  $t->assign('rss_items', $rss2_items);
 
   $t->display('rss.tpl');
   //http://www.phpeveryday.com/articles/Smarty-Variable-Associative-Arrays-P611.html
