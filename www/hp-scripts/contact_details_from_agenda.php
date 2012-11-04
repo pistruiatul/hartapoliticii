9e6955f950ab1e6fe751a39dcd62fa70d9c8ab3d
Octavian Costache <octavian.costache@gmail.com>
2012-11-04 14:47:23 -0500

    Fixing some more scripts

--- www/hp-scripts/contact_details_from_agenda.php
+++ www/hp-scripts/contact_details_from_agenda.php
@@ -104,20 +104,6 @@ function getCanonicalWebsite($address) {
   return $address;
 }
 
-function startsWith($haystack, $needle) {
-  $length = strlen($needle);
-  return (substr($haystack, 0, $length) === $needle);
-}
-
-function endsWith($haystack, $needle) {
-  $length = strlen($needle);
-  if ($length == 0) {
-      return true;
-  }
-
-  $start  = $length * -1; //negative
-  return (substr($haystack, $start) === $needle);
-}
 
 function infoFunction($person, $idString) {
   // do nothing.
