cd40c3ea8267f122f7b87cc532bf45c77363da49
Octavian Costache <octavian.costache@gmail.com>
2012-11-29 00:08:03 -0500

    A first draft for the importer of the România Curată new data. For now it crawls, parses and prints out the content.

---
+++ tools/romaniacurata_parser.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Export the current directory as a python path so we can include modules
+# from various python scripts as "ro.vivi.module_name"
+PYBIN='/usr/bin/python'
+
+PYTHONPATH="`pwd`/python/src"
+export PYTHONPATH
+
+$PYBIN python/src/ro/vivi/romaniacurata/crawl.py
