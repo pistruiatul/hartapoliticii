2a92e804e9b4854246c015f2a345cc7ebb1c09a6
Octavian Costache <octavian.costache@gmail.com>
2013-01-14 12:24:55 -0500

    Adding caching to the expanded news module which is also quite crawled because it usually has content

--- www/mods/news_expanded.php
+++ www/mods/news_expanded.php
@@ -1,22 +1,24 @@
 <?php
 
 $t = new Smarty();
+$t->caching = 1;
 
-// The list of news is here.
-$start = (int)$_GET['start'];
+if (!$t->is_cached('mod_news_expanded.tpl', $person->id)) {
+  // The list of news is here.
+  $start = (int)$_GET['start'];
 
-$t->assign('next', $start + 10);
-$t->assign('prev', $start - 10);
+  $t->assign('next', $start + 10);
+  $t->assign('prev', $start - 10);
 
-$t->assign('id', $person->id);
-$t->assign('name', $person->getUrlName());
-$t->assign('news', $person->getMostRecentNewsItems(10, $start));
+  $t->assign('id', $person->id);
+  $t->assign('name', $person->getUrlName());
+  $t->assign('news', $person->getMostRecentNewsItems(10, $start));
 
-// Top associations are supposed to go here.
-$associates = $person->getTopNewsAssociates(7);
-$t->assign('assoc', $associates);
-$t->assign('total_news', $associates[0]['cnt']);
-
-$t->display('mod_news_expanded.tpl');
+  // Top associations are supposed to go here.
+  $associates = $person->getTopNewsAssociates(7);
+  $t->assign('assoc', $associates);
+  $t->assign('total_news', $associates[0]['cnt']);
+}
+$t->display('mod_news_expanded.tpl', $person->id);
 
 ?>
