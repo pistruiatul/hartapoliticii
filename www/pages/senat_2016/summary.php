2d33a4c94218d7c1a0d9393a3532ef5298270b99
Octavian Costache <octavian.costache@gmail.com>
2016-12-05 20:50:40 -0500

    Also updating this other hacked up page

--- www/pages/senat_2016/summary.php
+++ www/pages/senat_2016/summary.php
@@ -2,10 +2,16 @@
 include_once('pages/functions_common.php');
 include_once('pages/cdep_2008/functions.php');
 include_once('hp-includes/electoral_colleges.php');
+include_once('hp-includes/elections_2016.php');
 
 $t = new Smarty();
+$colleges = getElectoralCollegeHashByCounty('senat');
+foreach ($colleges as $key => $col) {
+  $col['seats'] = $seats[ucwords($key)];
+  $colleges[$key] = $col;
+}
 
-$t->assign("colleges", getElectoralCollegeHashByCounty('senat'));
+$t->assign("colleges", $colleges);
 
 $t->display('cdep_2016_summary.tpl');
 ?>
